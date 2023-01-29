843fd7a7468e8c2aea6ead95b6026b6dcea2f2da
oskark22 <oskar.koskiranta@gmail.com>
2023-01-29 17:24:06 +0200

    Added some code

--- src/main/java/ie/atu/semester1/revision.java
+++ src/main/java/ie/atu/semester1/revision.java
@@ -1,4 +1,11 @@
 package ie.atu.semester1;
 
+import java.util.Scanner;
+
 public class revision {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your sales: ");
+        double sales = keyboard.nextDouble();
+    }
 }
